a409573166d4b9fad1d976bc82921df662d0497f
Roman Volodin <roman.volodin@gmail.com>
2023-07-26 15:06:51 +0300

    Add index page

--- src/catalog/urls.py
+++ src/catalog/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+
+from catalog import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
--- src/catalog/views.py
+++ src/catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Author, Book, BookInstance, Genre
+
+
+def index(request):
+    book_count = Book.objects.count()
+    instance_count = BookInstance.objects.count()
+    available_instance_count = BookInstance.objects.filter(status__exact="a").count()
+    author_count = Author.objects.count()
+    genre_count = Genre.objects.count()
+    astronaut_count = Book.objects.filter(title__icontains="Астронавт").count()
+
+    context = {
+        "book_count": book_count,
+        "instance_count": instance_count,
+        "available_instance_count": available_instance_count,
+        "author_count": author_count,
+        "genre_count": genre_count,
+        "astronaut_count": astronaut_count,
+    }
+    return render(request, "catalog/index.html", context)
